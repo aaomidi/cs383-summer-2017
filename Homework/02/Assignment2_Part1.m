f05f2253a83d0b201013b52e88b5a8e0c5cb39ca
Amir <amir@aaomidi.com>
2017-07-26 15:48:35 -0400

    A bit of work.

--- Homework/02/Assignment2_Part1.m
+++ Homework/02/Assignment2_Part1.m
@@ -22,7 +22,7 @@ end
 std = standardize(data(:,2:end));
 newData = [data(:,1), std];
 
-otherData = kmeme(newData,2,7,6);
+otherData = kmeme(newData,2,2,3);
 
 function [X] = kmeme(X, k, xcol, ycol)
     s = RandStream('mt19937ar','Seed',0);
@@ -36,10 +36,9 @@ function [X] = kmeme(X, k, xcol, ycol)
     
     kVals = X(1:k, :);
     
-    plot(d1,d2,'rx');
-    hold on;
-    
-    plot(kVals(:,xcol),kVals(:,ycol), 'bo');
+    %plot(d1,d2,'rx');
+    %hold on;
+    %plot(kVals(:,xcol),kVals(:,ycol), 'bo');
     
     maxY = size(X,2);
     
@@ -60,8 +59,29 @@ function [X] = kmeme(X, k, xcol, ycol)
             [M, I] = min(distances);
             X(c, end) = I;
         end
-        return
+        last = size(X,2);
+        for i = 1:size(kVals,1)
+            allData = (X(X(:,last)==i,1:end-1));
+            disp(i);
+            kVals(i,:) = mean(allData);
+        end
+        
+        if k==2 
+            break
+        end
     end
+    
+    d1x = X(X(:,end)==1, xcol);
+    d1y = X(X(:,end)==1, ycol);
+    
+    d2x = X(X(:,end)==2, xcol);
+    d2y = X(X(:,end)==2, ycol);
+    
+    plot(d1x,d1y,'rx');
+    hold on;
+    plot(d2x,d2y,'bo');
+    hold on;
+    plot(kVals(:,xcol),kVals(:,ycol), 'bo');
 end
 
 % Standardizes the data input
